0c5d5fcdd0a8ae349a9981701fd938c8d3c0429f
Bendik Samseth <b.samseth@gmail.com>
2019-01-13 01:01:53 +0100

    Add reference definitions for convenience.

--- include/definitions.hpp
+++ include/definitions.hpp
@@ -6,4 +6,10 @@ using Real = double;
 
 using Matrix = Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 using Array  = Eigen::Array<Real, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-using Vector = Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic>;
+using Vector = Eigen::Matrix<Real, Eigen::Dynamic, 1>;
+using RowVector = Eigen::Matrix<Real, 1, Eigen::Dynamic>;
+
+using MatrixRef = Eigen::Ref<Matrix>;
+using ArrayRef = Eigen::Ref<Array>;
+using VectorRef = Eigen::Ref<Vector>;
+using RowVectorRef = Eigen::Ref<RowVector>;
